139d9fda6869a78cdee824a6be3ba8ee0bca6ea0
Abdelrahman Hassany <68061891+Abdelrahman-Hassany@users.noreply.github.com>
2025-04-23 02:33:53 +0200

    fix some error

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -26,7 +26,7 @@ load_dotenv(BASE_DIR / '.env',override=True)
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
